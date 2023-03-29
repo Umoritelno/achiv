f4adc9bf220a818fa5de90a442e8e856e65cbe14
Umoritelno <125469661+Umoritelno@users.noreply.github.com>
2023-03-29 11:12:07 +0300

    Add files via upload

--- lua/autorun/server/sv_achiv.lua
+++ lua/autorun/server/sv_achiv.lua
@@ -21,6 +21,8 @@ end
 
 net.Receive("InitAchiv",function(len,ply)
     net.Start("ClientInitAchiv")
+    ply.achivki = util.JSONToTable(ply:GetPData("Achiv",util.TableToJSON(newachivki)))
+    --PrintTable(ply.achivki)
     --net.WriteTable(util.JSONToTable(ply:GetPData("Achiv",util.TableToJSON(achivki))))
     --net.WriteString("Byu")
     --net.WriteString(util.Compress(util.TableToJSON(ply:GetPData("Achiv",achivki))))
@@ -62,8 +64,20 @@ hook.Add("PlayerSpawn","Achiv",function(ply)
 end)
 
 hook.Add("PlayerHurt","Yablocko",function(victim,attacker,remain,taken)
-    if remain <= 0 then
+    if remain <= 0 and victim:LastHitGroup() == 1 then
         net.Start("headAchiv")
         net.Send(attacker)
     end
+end)
+
+hook.Add("PlayerSay","showerachiv",function(sender,text,tm)
+    if string.lower(text) == "noclip into the breach shower" then 
+        net.Start("showerAchiv")
+        net.Send(sender)
+    end
+end)
+
+hook.Add("PlayerDeath","DieAchiv",function(victim,inflictor,attacker)
+    net.Start("DieAchiv")
+    net.Send(victim)
 end)
--- lua/autorun/sh_achiv.lua
+++ lua/autorun/sh_achiv.lua
@@ -2,44 +2,64 @@ achivki = {}
 local plym = FindMetaTable("Player")
 
 
-
 function AddAchiv(data)
     achivki[table.Count(achivki) + 1] = data
 end
 
 AddAchiv({
-    name = "I am the cure",
-    description = "Find a secret item on map and kill someone by him",
+    name = "Welcome to server Анальные булки",
+    description = "Join to our community",
     icon = "serverlogo.png",
     nets = "CureAchiv",
-    unlocked = false,
+    secret = false,
 })
 
 AddAchiv({
-    name = "Headshot",
-    description = "Kill someone in the head",
-    icon = "serverlogo.png",
+    name = "Apple",
+    description = "Blow someones apple",
+    icon = "headshot.png",
     nets = "headAchiv",
-    unlocked = false,
+    secret = false,
 })
+
+AddAchiv({
+    name = "Secret Phrase",
+    description = "shower",
+    icon = "shower.png",
+    nets = "showerAchiv",
+    secret = true,
+})
+
+AddAchiv({
+    name = "Sooner or later",
+    description = "This cannot be avoided",
+    icon = "Die.jpg",
+    nets = "DieAchiv",
+    secret = false,
+})
+
+newachivki = {}
+for i = 1, table.Count(achivki) do
+    newachivki[i] = false
+end
 
 function plym:UnlockAchiv(id)
     local scrw,scrh = ScrW(),ScrH()
     if not achivki[id] then print("ERROR TO GIVE ACHIV") return end 
     --if not self.achivki then self.achivki = util.TableToJSON(achivki) end 
     local jsontable = LocalPlayer().achivki
-    local iconmat = Material(jsontable[id].icon)
-    if jsontable[id].unlocked == true then return end 
-    jsontable[id].unlocked = true
+    local iconmat = Material(achivki[id].icon)
+    if jsontable[id] == true then return end 
+    jsontable[id] = true
     self:SetPData("Achiv",util.TableToJSON(jsontable))
-    LocalPlayer().achivki = util.JSONToTable(LocalPlayer():GetPData("Achiv",util.TableToJSON(achivki)))
+    LocalPlayer().achivki = util.JSONToTable(LocalPlayer():GetPData("Achiv",util.TableToJSON(newachivki)))
     net.Start("Unlock")
-    net.WriteString(self:GetPData("Achiv",util.TableToJSON(achivki)))
+    net.WriteString(self:GetPData("Achiv",util.TableToJSON(newachivki)))
     net.SendToServer()
     local notice = vgui.Create("DPanel")
     notice:SetPos(scrw * 1,scrh * 0.25)
     notice:SetSize(scrw * 0.1,scrh * 0.1)
-    notice:SetToolTip(jsontable[id].description)
+    notice:SetToolTip(achivki[id].description)
     --[[notice.Paint = function(s,w,h)
         draw.RoundedBox(0,0,0,w,h * 0.9,Color(255,255,255))
         surface.SetDrawColor( 255, 255, 255 )
@@ -75,20 +95,20 @@ end
 end 
 
 function plym:LockAchiv(id) 
-    local jsontable = util.JSONToTable(self.achivki)
-    if jsontable[id].unlocked == false then return end 
-    jsontable[id].unlocked = false 
+    local jsontable = self.achivki
+    if jsontable[id] == false then return end 
+    jsontable[id] = false 
     self:SetPData("Achiv",util.TableToJSON(jsontable))
-    LocalPlayer().achivki = util.JSONToTable(LocalPlayer():GetPData("Achiv",util.TableToJSON(achivki)))
+    LocalPlayer().achivki = util.JSONToTable(LocalPlayer():GetPData("Achiv",util.TableToJSON(newachivki)))
     net.Start("Lock")
-    net.WriteString(self:GetPData("Achiv",util.TableToJSON(achivki)))
+    net.WriteString(self:GetPData("Achiv",util.TableToJSON(newachivki)))
     net.SendToServer()
 end 
 
 function plym:ResetAchiv()
-    LocalPlayer():SetPData("Achiv",util.TableToJSON(achivki))
-    LocalPlayer().achivki = util.JSONToTable(LocalPlayer():GetPData("Achiv",util.TableToJSON(achivki)))
+    LocalPlayer():SetPData("Achiv",util.TableToJSON(newachivki))
+    LocalPlayer().achivki = util.JSONToTable(LocalPlayer():GetPData("Achiv",util.TableToJSON(newachivki)))
     net.Start("Reset")
-    net.WriteString(self:GetPData("Achiv",util.TableToJSON(achivki)))
+    net.WriteString(self:GetPData("Achiv",util.TableToJSON(newachivki)))
     net.SendToServer()
 end 
